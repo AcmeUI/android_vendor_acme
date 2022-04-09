0285601d89a429b7eca3c6aa4388232e781aa2de
Danny Lin <danny@kdrag0n.dev>
2022-04-10 01:52:21 +0800

    overlay: Add product overlay
    * We're going to move most of our config changes here for portability.
    
    Change-Id: I4094c08451858e7ba668f50cf5bb4256ee3fef40

--- build/init.mk
+++ build/init.mk
@@ -6,6 +6,7 @@
 
 # Inherit vendor submodules
 $(call inherit-product, vendor/acme/bootanimation/bootanimation.mk)
+$(call inherit-product, vendor/acme/overlay/overlay.mk)
 
 # Acme version
 -include vendor/acme/build/tools/version.mk
---
+++ overlay/overlay.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 Acme
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Vendor overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/acme/overlay
