e6e5d6a31a18d12e419b458facea40c492e52059
XayahSuSuSu <zds1249475336@gmail.com>
2022-04-10 01:36:10 +0800

    build: Add modular build hooks
    * Ref: https://github.com/ProtonAOSP/android_vendor_proton
    
    Change-Id: If8ae32b04f674fbf5af37c4f8f84bbba390cb523

---
+++ build/init.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 Acme
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Acme version
+-include vendor/acme/build/tools/version.mk
---
+++ build/tools/version.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 Acme
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ACME_VERSION := 12.1
+
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.version.custom=$(ACME_VERSION)
