9d5dc9612fb3de30c246718a6513c15b658cfa89
TH779 <i@779.moe>
2022-04-22 18:49:45 +0800

    acme: Checkout apns-conf.xml from LineageOS
    * From LineageOS/android_vendor_lineage@2007edd.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I3d02676399454e4f7116fb627e8cc994e206cd6f

--- config/mobile.mk
+++ config/mobile.mk
@@ -7,6 +7,10 @@
 # Inherit common Acme stuff
 $(call inherit-product, vendor/acme/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/acme/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
