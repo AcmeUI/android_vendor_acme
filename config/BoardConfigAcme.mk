8291adc4e5f0ee628cd8fa036e66794dd26d51bb
Rashed Abdel-Tawab <rashed@linux.com>
2022-04-22 17:25:11 +0800

    build: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- config/BoardConfigAcme.mk
+++ config/BoardConfigAcme.mk
@@ -1,5 +1,10 @@
 # Kernel
 include vendor/acme/config/BoardConfigKernel.mk
 
+# Qcom-specific bits
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/acme/config/BoardConfigQcom.mk
+endif
+
 # Soong
 include vendor/acme/config/BoardConfigSoong.mk
