dfde9c3cb7eb8350714fd480634665c181dc97c8
Xayah <zds1249475336@gmail.com>
2022-05-27 01:38:32 +0800

    config: Add ACME_GMS_TYPE in ACME_VERSION
    Change-Id: Id24f04ce0aae7b328f51c603dc6aa786aeeadbb4

--- config/version.mk
+++ config/version.mk
@@ -14,19 +14,22 @@ ifeq ($(IS_RELEASE), true)
     ACME_BUILD_TYPE := RELEASE
 endif
 
-ACME_VERSION := AcmeUI-$(ACME_BRANCH)-$(ACME_DEVICE)-$(ACME_BUILD_TYPE)-$(ACME_DATE)
-
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.acme.branch=$(shell echo "$(ACME_BRANCH)") \
     ro.acme.build.type=$(shell echo "$(ACME_BUILD_TYPE)")
 
 ifeq ($(WITH_GMS), true)
+ACME_GMS_TYPE := GMS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.acme.gms.type=GMS
 else ifeq ($(WITH_GMS_CORE), true)
+ACME_GMS_TYPE := GMSCore
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.acme.gms.type=GMSCore
 else
+ACME_GMS_TYPE := VANILLA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.acme.gms.type=VANILLA
 endif
+
+ACME_VERSION := AcmeUI-$(ACME_BRANCH)-$(ACME_DEVICE)-$(ACME_GMS_TYPE)-$(ACME_BUILD_TYPE)-$(ACME_DATE)
