bb67905c8464312d9b00d87bb7834224ad5c351d
tarsin <yuanqingxiang233@163.com>
2023-08-02 13:31:27 +0800

    config: Don't flatten APEXs
    Android 14 will remove Flatten APEXs support

--- config/common.mk
+++ config/common.mk
@@ -17,9 +17,6 @@ $(call inherit-product, vendor/acme/overlay/overlay.mk)
 # Sounds
 include vendor/acme/config/sounds.mk
 
-# Flatten APEXs for performance
-OVERRIDE_TARGET_FLATTEN_APEX := true
-
 # This needs to be specified explicitly to override ro.apex.updatable=true from
 # prebuilt vendors, as init reads /product/build.prop after /vendor/build.prop
 PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
